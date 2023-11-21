bf3bb216a806623af71c22e0dbbcce56617380af
Simon Atkins <simon@vault.fan>
2023-11-21 18:07:17 +0000

    Update to next13 and Remove .babelrc for compatibility

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: {
+    styledComponents: {
+      displayName: process.env.NODE_ENV === "development",
+      ssr: true,
+      fileName: false,
+      topLevelImportPaths: ["@emotion/react", "@emotion/styled"],
+      cssProp: true,
+      // Not supported yet.
+      // minify?: boolean,
+      // Not supported yet.
+      // transpileTemplateLiterals?: boolean,
+      // Not supported yet.
+      // pure?: boolean,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import "../app.css";
+import { Poppins } from "@next/font/google";
+
+const fonts = Poppins({
+  weight: ["400", "500", "600"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={fonts.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
